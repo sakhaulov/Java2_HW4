3560ef9535ba5fe1fbc49bd7f12dcfdbe77acc0f
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2022-03-24 22:25:08 +0500

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         //Первое задание
         String[] arr = {"раз", "два", "три", "два", "три", "три", "четыре", "пять", "шесть", "семь"};
-        System.out.println(UniqueValues.countUniqueValues(arr)+"\n");
+        System.out.println("Считаем уникальные значения: \n" + UniqueValues.countUniqueValues(arr)+"\n");
 
 
         //Второе задание
@@ -15,9 +15,9 @@ public class Main {
         phonebook.add(125, "Антон");
         phonebook.add(126, "Антон");
 
-        System.out.println("Номера пользователей по имени: 'Антон'\n" + phonebook.get("Антон"));
-        System.out.println("Номера пользователей по имени: 'Тимур'\n" + phonebook.get("Тимур"));
-        System.out.println("Номера пользователей по имени: 'Артём'\n" + phonebook.get("Артём"));
-        System.out.println("Номера пользователей по имени: 'Егор'\n" + phonebook.get("Егор"));
+        System.out.println("Номера пользователей по имени 'Антон':\n" + phonebook.get("Антон"));
+        System.out.println("Номера пользователей по имени 'Тимур':\n" + phonebook.get("Тимур"));
+        System.out.println("Номера пользователей по имени 'Артём':\n" + phonebook.get("Артём"));
+        System.out.println("Номера пользователей по имени 'Егор':\n" + phonebook.get("Егор"));
     }
 }
